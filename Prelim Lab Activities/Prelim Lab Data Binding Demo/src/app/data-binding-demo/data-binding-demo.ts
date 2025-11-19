d0e5a3dd5db7afda32b8a1d5650b7dd8e8c73b6c
Royce Simbillo <vincent.simbillo@gmail.com>
2025-11-19 15:43:38 +0800

    Add files via upload

---
+++ Prelim Lab Activities/Prelim Lab Data Binding Demo/src/app/data-binding-demo/data-binding-demo.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding-demo',
+  imports: [],
+  templateUrl: './data-binding-demo.html',
+  styleUrl: './data-binding-demo.css',
+})
+export class DataBindingDemo {
+  textColor = 'blue';
+}
+
